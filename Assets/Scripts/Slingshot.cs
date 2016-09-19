1058d51c3a3149fa66c9dfc721dc49fce940dc60
dthoma6403 <dthoma6403@gmail.com>
2016-09-19 14:13:10 -0700

    Instantiated Projectile

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -2,12 +2,39 @@
 using System.Collections;
 
 public class Slingshot : MonoBehaviour {
+	//fields set in the Unity Insector pane
+	public GameObject 	prefabProjectile;
+	public bool 		________________;
+	// fields are set dynamically
+	public GameObject	launchPoint;
+	public Vector3		launchPos;
+	public GameObject	projectile;
+	public bool			aimingMode;
+
+	void Awake() {
+		Transform launchPointTrans = transform.Find ("LaunchPoint");
+		launchPoint = launchPointTrans.gameObject;
+		launchPoint.SetActive (false);
+		launchPos = launchPointTrans.position;
+	}
 
 	void OnMouseEnter() {
-		print ("Slingshot:OnMouseEnter()");
+		//print ("Slingshot:OnMouseEnter()");
 	}
 
 	void OnMouseExit() {
-		print ("Slingshot:OnMouseExit()");
+		//print ("Slingshot:OnMouseExit()");
+	}
+
+	void OnMouseDown() {
+		// The player has pressed the mouse button while over slingshot
+		aimingMode = true;
+		// Insantiate a projectile
+		projectile = Instantiate (prefabProjectile) as GameObject;
+		// Start at the launchPoint
+		projectile.transform.position = launchPos;
+
+		// Set it to isKinematic for now
+		projectile.GetComponent<Rigidbody>().isKinematic = true;
 	}
 }
