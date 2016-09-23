5b2aa8ef46c233259933f0e591f5aba5b8b0e7be
dthoma6403 <dthoma6403@gmail.com>
2016-09-23 11:53:25 -0700

    ProjectileLine and Scripting

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 
 public class Slingshot : MonoBehaviour {
+	static public Slingshot    S;
 	// fields set in the Unity Inspector pane
 	public GameObject        prefabProjectile;
 	public float             velocityMult = 4f;
@@ -43,6 +44,8 @@ public class Slingshot : MonoBehaviour {
 	}
 	
 	void Awake() {
+		// Set the Slingshot singleton S
+		S = this;
 		Transform launchPointTrans = transform.Find("LaunchPoint");
 		launchPoint = launchPointTrans.gameObject;
 		launchPoint.SetActive( false );
