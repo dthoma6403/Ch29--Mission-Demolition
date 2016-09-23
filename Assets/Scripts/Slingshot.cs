c26f2e65d4959647d59235af7d29c05d43bca5fc
dthoma6403 <dthoma6403@gmail.com>
2016-09-23 12:40:11 -0700

    Added Level and Shot Increments / Castle Levels

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -40,6 +40,7 @@ public class Slingshot : MonoBehaviour {
 			projectile.GetComponent<Rigidbody>().velocity = -mouseDelta * velocityMult;
 			FollowCam.S.poi = projectile;
 			projectile = null;
+			MissionDemolition.ShotFired();
 		}
 	}
 	
